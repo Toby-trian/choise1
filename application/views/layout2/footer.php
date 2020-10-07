e35de070762dbe266ca3ff8d2d914d33a224333c
dhikiwibawa <70878601+dhikiwibawa@users.noreply.github.com>
2020-10-07 15:34:12 +0700

    footerbaru

--- application/views/layout2/footer.php
+++ application/views/layout2/footer.php
@@ -1,17 +1,17 @@
 <div class="footer py-5 text-center">
   <div class="container">
-    <div class="row mb-5">
+    <div class="row mb-3">
       <div class="col-md-6 col-sm-12">
         <div style='text-align:right;'>
           <img src="<?php echo base_url('assets2/images/companylogohighresfooterjpg.jpg') ?>" alt="" style="width:40%;">
         </div>
       </div>
     </div>
-    <div class="col-md-6 col-sm-12">
+    <div class="row">
       <div class="col-md-6 col-sm-12">
         <div style='text-align:right;'>
           <p class="mb-0" style='text-align:right;'>
-              Chaakra Consulting menggunakan Chaakra Online System (Choise) untuk melakukan kegiatan rekrutmen calon pegawai bagi perusahaan Chaakra Consuting maupun perusahaan lain yang menggunakan jasa asesmen dan rekrutmen pada Chaakra Consulting.
+            Chaakra Consulting menggunakan Chaakra Online System (Choise) untuk melakukan kegiatan rekrutmen calon pegawai bagi perusahaan Chaakra Consuting maupun perusahaan lain yang menggunakan jasa asesmen dan rekrutmen pada Chaakra Consulting.
           </p>
         </div>
       </div>
@@ -26,16 +26,18 @@
         <p class="mb-0" style='text-align:left;'>Gedung Virto Office, Ruko Galaxi Bumi Permai J1-23A Surabaya</p>
       </div>
     </div>
-    <div class="row">
-      <div class="col-12">
-        <p class="mb-0">
-          <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-          Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
-          <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-        </p>
-      </div>
+  </div><br>
+  
+  <div class="row">
+    <div class="col-12">
+      <p class="mb-0">
+        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a> | Edited by Tim IT Chaakra Consulting
+        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+      </p>
     </div>
   </div>
+</div>
 </div><!-- .site-wrap -->
 
 <script src="<?php  echo base_url('assets2/js/jquery-3.3.1.min.js') ?>"></script>
@@ -51,22 +53,22 @@
 <script src="<?php  echo base_url('assets2/js/jquery.sticky.js') ?>"></script>
 <script src="<?php  echo base_url('assets2/js/main.js') ?>"></script>
 
-  <script>
-    (function($) {
-      'use strict';
-      
-      jQuery(document).on('ready', function(){
-      
-          $('a.page-scroll').on('click', function(e){
-            var anchor = $(this);
-            $('html, body').stop().animate({
-              scrollTop: $(anchor.attr('href')).offset().top - 50
-            }, 1500);
-            e.preventDefault();
-          });
-      });        
-    })(jQuery);
-  </script>
-  
-  </body>
+<script>
+  (function($) {
+    'use strict';
+
+    jQuery(document).on('ready', function(){
+
+      $('a.page-scroll').on('click', function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+          scrollTop: $(anchor.attr('href')).offset().top - 50
+        }, 1500);
+        e.preventDefault();
+      });
+    });        
+  })(jQuery);
+</script>
+
+</body>
 </html>
