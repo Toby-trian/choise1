7aa19c39922271bc5ef979a9bb6be83be37ec872
Edwin Heriyansyah <63455162+edwinheriyansyah@users.noreply.github.com>
2020-10-23 13:44:38 +0700

    Update frame_ujian2.php

--- application/views/pelamar/ujian/frame_ujian2.php
+++ application/views/pelamar/ujian/frame_ujian2.php
@@ -11,35 +11,35 @@
 				
 				<div class="form-check col-sm-2 text-center" style="margin-top: 5px;">
 					<label class="form-check-label" for="cfit113">a</label>
-						<input class="form-check-input" type="checkbox" <?php if(!empty($jawaban2->jawaban) && $jawaban2->jawaban== "a"){?> checked="checked" <?php } ?> name="jawaban" value="a">
+						<input class="form-check-input" type="checkbox" <?php if(!empty($jawaban2->jawaban) && $jawaban2->jawaban== "a"){?> checked="checked" <?php } ?> name="jawaban[]" value="a">
 					<center>
 						<img src="<?php echo ($soal_subtes2->opsi_a != '' ? base_url('./upload/bank_soal/cfit/' . $soal_subtes2->opsi_a) : base_url('./upload/bank_soal/img_default.jpg')); ?>" class="img-responsive" alt="" style="width: 50px; border-radius: 5px;">
 					</center>
 				</div>
 				<div class="form-check col-sm-2 text-center" style="margin-top: 5px;">
 					<label class="form-check-label" for="cfit113">b</label>
-					<input class="form-check-input" type="checkbox"  <?php if(!empty($jawaban2->jawaban) && $jawaban2->jawaban== "b"){?> checked="checked" <?php } ?> name="jawaban" value="b">
+					<input class="form-check-input" type="checkbox"  <?php if(!empty($jawaban2->jawaban) && $jawaban2->jawaban== "b"){?> checked="checked" <?php } ?> name="jawaban[]" value="b">
 					<center>
 						<img src="<?php echo ($soal_subtes2->opsi_b != '' ? base_url('./upload/bank_soal/cfit/' . $soal_subtes2->opsi_b) : base_url('./upload/bank_soal/img_default.jpg')); ?>" class="img-responsive" alt="" style="width: 50px; border-radius: 5px;">
 					</center>
 				</div>
 				<div class="form-check col-sm-2 text-center" style="margin-top: 5px;">
 					<label class="form-check-label" for="cfit113">c</label>
-					<input class="form-check-input" type="checkbox"  <?php if(!empty($jawaban2->jawaban) && $jawaban2->jawaban== "c"){?> checked="checked" <?php } ?> name="jawaban" value="c">
+					<input class="form-check-input" type="checkbox"  <?php if(!empty($jawaban2->jawaban) && $jawaban2->jawaban== "c"){?> checked="checked" <?php } ?> name="jawaban[]" value="c">
 					<center>
 						<img src="<?php echo ($soal_subtes2->opsi_c != '' ? base_url('./upload/bank_soal/cfit/' . $soal_subtes2->opsi_c) : base_url('./upload/bank_soal/img_default.jpg')); ?>" class="img-responsive" alt="" style="width: 50px; border-radius: 5px;">
 					</center>
 				</div>
 				<div class="form-check col-sm-2 text-center" style="margin-top: 5px;">
 					<label class="form-check-label" for="cfit113">d</label>
-					<input class="form-check-input" type="checkbox"  <?php if(!empty($jawaban2->jawaban) && $jawaban2->jawaban== "d"){?> checked="checked" <?php } ?> name="jawaban" value="d">
+					<input class="form-check-input" type="checkbox"  <?php if(!empty($jawaban2->jawaban) && $jawaban2->jawaban== "d"){?> checked="checked" <?php } ?> name="jawaban[]" value="d">
 					<center>
 						<img src="<?php echo ($soal_subtes2->opsi_d != '' ? base_url('./upload/bank_soal/cfit/' . $soal_subtes2->opsi_d) : base_url('./upload/bank_soal/img_default.jpg')); ?>" class="img-responsive" alt="" style="width: 50px; border-radius: 5px;">
 					</center>
 				</div>
 				<div class="form-check col-sm-2 text-center" style="margin-top: 5px;">
 					<label class="form-check-label" for="cfit113">e</label>
-					<input class="form-check-input" type="checkbox"  <?php if(!empty($jawaban2->jawaban) && $jawaban2->jawaban== "e"){?> checked="checked" <?php } ?> name="jawaban" value="e">
+					<input class="form-check-input" type="checkbox"  <?php if(!empty($jawaban2->jawaban) && $jawaban2->jawaban== "e"){?> checked="checked" <?php } ?> name="jawaban[]" value="e">
 					<center>
 						<img src="<?php echo ($soal_subtes2->opsi_e != '' ? base_url('./upload/bank_soal/cfit/' . $soal_subtes2->opsi_e) : base_url('./upload/bank_soal/img_default.jpg')); ?>" class="img-responsive" alt="" style="width: 50px; border-radius: 5px;">
 					</center>
